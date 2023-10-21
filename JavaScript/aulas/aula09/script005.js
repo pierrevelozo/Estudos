499e70795f81ba78449177bfa687e9e6f3ffe07a
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-10-21 20:57:39 -0300

    Atualizando JS da aula09

---
+++ JavaScript/aulas/aula09/script005.js
@@ -0,0 +1,10 @@
+
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[0]
+/*
+var d = window.document.getElementById('msg')
+window.document.write('Está escrito assim: ' + p1.innerText)
+d.style.background = 'green';
+d.innerText = 'Estou aguardando' 
+*/
+window.document.querySelector('div#msg').style.color = 'blue'
