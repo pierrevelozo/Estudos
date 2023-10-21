43b0ce4ff0316f9bd2907b33941a6e53e8a66bcf
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-10-21 20:53:12 -0300

    Atualizando JS da aula05

---
+++ JavaScript/aulas/aula06/script003.js
@@ -0,0 +1,5 @@
+
+var n1 = Number(window.prompt('Digite um número: '));
+var n2 = Number(window.prompt('Digite outro número: '));
+
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${n1 + n2}`);
---
+++ JavaScript/aulas/aula06/script004.js
@@ -0,0 +1,5 @@
+
+var nome = window.prompt('Qual seu nome?');
+document.write(`Olá, ${nome}. Seu nome tem ${nome.length} letras.<br>`);
+document.write(`${nome} me maiúsculas é ${nome.toUpperCase()}<br>`);
+document.write(`${nome} em minúsculas é ${nome.toLowerCase()}`);
