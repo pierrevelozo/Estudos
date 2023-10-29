09ad6a94c69a5c28a3429d86e6dacde23b3db1b2
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-10-29 00:01:05 -0300

    Criando o programa Mario do CS50

---
+++ CS50/mario/mario.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int w = 0; w < 3; w++)
+    {
+        for (int h = 0; h < 3; h++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
