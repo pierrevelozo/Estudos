fbd517b2b752d81434461a3f81fae66e9e242cb0
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-10-28 22:07:22 -0300

    Criando o programa Meow do CS50

---
+++ CS50/meow/meow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
+
+int main(void)
+{
+    int n = get_int("Repetir quantas vezes? ");
+
+    meow(n);
+}
