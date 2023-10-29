5e4afa08bccfe982d921fc5522166d3b09495100
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-10-29 18:45:14 -0300

    Atualizando o exercício Cash do CS50

---
+++ CS50/credit/credit.c
@@ -0,0 +1,32 @@
+#include "cs50.h"
+#include <stdio.h>
+
+long obter_num(void);
+
+
+int main(void)
+{
+    long num = obter_num();
+
+    long result1 = num % 1000;
+
+    long result2 = num % 100;
+
+    long result3 = result1 - result2;
+    
+    printf("%li\n", result3);
+
+}
+
+
+long obter_num(void)
+{
+    long n;
+    do
+    {
+        n = get_long("Número: ");
+    } 
+    while (false);
+    
+    return n;
+}
