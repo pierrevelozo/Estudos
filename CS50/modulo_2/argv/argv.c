a4ebab662ebcefbee5753881d6dfd9aa22e549d3
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-11-11 19:42:25 -0300

    Atualizando o programa Argv do CS50 - Escrevendo o argumento quebrando linhas

--- CS50/modulo_2/argv/argv.c
+++ CS50/modulo_2/argv/argv.c
@@ -1,14 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     if (argc == 2)
     {
-        printf("hello, %s\n", argv[1]);
-    }
-    else
-    {
-        printf("Hello, World\n");
+        for (int i = 0, n = strlen(argv[1]); i < n; i++)
+        {
+            printf("%c\n", argv[1][i]);
+        }
     }
 }
