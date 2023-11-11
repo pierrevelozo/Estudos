ec298d25c4a2a356c9fef0bf7131cb49df7e526c
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-11-11 17:09:47 -0300

    Atualizando o programa String do CS50 - Usando a biblioteca String.h

--- CS50/modulo_2/string/string.c
+++ CS50/modulo_2/string/string.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string s = get_string("Input: ");
     printf("Output: ");
 
-    for (int i = 0; s[i] != '\0'; i++)
+    for (int i = 0; i < strlen(s); i++)
     {
         printf("%c", s[i]);
     }
