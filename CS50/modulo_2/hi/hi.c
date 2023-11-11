2a62a19b82d1c52469a1fb66f80f3dfdd1a3c59a
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-11-11 16:41:12 -0300

    Atualizando o programa Hi do CS50 - Olhando espaços na memória

--- CS50/modulo_2/hi/hi.c
+++ CS50/modulo_2/hi/hi.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
     string s = "HI!";
     
-    printf("%i %i %i\n", s[0], s[1], s[2]);
+    printf("%i %i %i %i %i\n", s[0], s[1], s[2], s[3], s[40]);
 }
