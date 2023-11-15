547a3cc2e37c565ce749ad45f85e855d0294a9f2
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-11-15 20:22:17 -0300

    Melhorando o programa Phonebook do CS50

--- CS50/modulo_3/phonebook/phonebook.c
+++ CS50/modulo_3/phonebook/phonebook.c
@@ -2,16 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    string names[] = {"Brian", "David"};
-    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+typedef struct{
+    string name;
+    string number;
+} person;
 
-    for (int i = 0; i < 2; i++){
-        if (strcmp(names[i], "David") == 0){
-            printf("Found %s\n", numbers[i]);
+const int NUMBER = 2;
+
+int main(void){
+    person people[NUMBER];
+
+    people[0].name = "Brian";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
+
+    for (int i = 0; i < NUMBER; i++){
+        people[i].name = get_string("Name: ");
+        people[i].number = get_string("Phone Number: ");
+    }
+
+    string name = get_string("Search Name: ");
+
+    for (int i = 0; i < NUMBER; i++){
+        if (strcmp(people[i].name, name) == 0){
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
-    printf("Not found");
+    printf("Not found\n");
     return 1;
 }
