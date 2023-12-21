0d869933ef05028fa2a89e4a4ddeb61775af718b
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-12-21 01:32:19 -0300

    Atualizando o programa Scanf do módulo 4 do CS50.

--- CS50/modulo_4/scanf/scanf.c
+++ CS50/modulo_4/scanf/scanf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
-    int x;
-    printf("x: ");
-    scanf("%i", &x);
-    printf("x: %i\n", x);
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
 
     return 0;
 }
