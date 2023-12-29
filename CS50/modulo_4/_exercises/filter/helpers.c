0216ad89b48561b89b9ec452ab79f029653c0f5c
Henrique Velozo <henriquebrumvelozo@gmail.com>
2023-12-29 18:41:04 -0300

    Resolvendo a função Grayscale do exercício Filter do módulo 4 do CS50.

---
+++ CS50/modulo_4/_exercises/filter/helpers.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            //printf("Altura: %i\n", i);
+            //printf("Largura: %i\n", j);
+
+            //printf("R: %i\n", image[i][j].rgbtRed);
+            //printf("G: %i\n", image[i][j].rgbtGreen);
+            //printf("B: %i\n", image[i][j].rgbtBlue);
+
+            BYTE media = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+
+            image[i][j].rgbtGreen = media;
+            image[i][j].rgbtBlue = media;
+            image[i][j].rgbtRed = media;
+        }
+    }
+
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
